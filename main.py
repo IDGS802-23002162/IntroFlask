278da57e4d03115f1a08a98d3486622f7e43f02c
IDGS802-23002162 <86053@alumnos.utleon.edu.mx>
2026-01-14 13:59:44 -0600

    Agregadas rutas, templates y static en Flask

--- main.py
+++ main.py
@@ -1,10 +1,58 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, world!"
+def index():
+    titulo = "IDGS-802 Flask"
+    lista = ['Juan', 'Karla', 'Miguel', 'Ana']
+    return render_template('index.html', titulo=titulo, lista=lista)
+
+@app.route('/formularios')
+def formularios():
+    return render_template("formularios.html")
+
+@app.route('/reportes')
+def reportes():
+    return render_template("reportes.html")
+
+@app.route('/hola')
+def hola():
+    return "¡Hola, Hola!"
+
+@app.route('/user/<string:user>')
+def user(user):
+    return f"Hello, {user}!"
+
+@app.route('/numero/<int:n>')
+def numero(n):
+    return f"Número: {n}"
+
+@app.route("/user/<int:id>/<string:username>")
+def username(id, username):
+    return f"ID: {id} nombre: {username}"
+
+@app.route("/suma/<float:n1>/<float:n2>")
+def suma(n1, n2):
+    resultado = n1 + n2
+    return f"<h1>Calculadora</h1><p>La suma de {n1} + {n2} es: <strong>{resultado}</strong></p>"
+
+@app.route("/default/")
+@app.route("/default/<string:param>")
+def func2(param="Juan"):
+    return f"<h1>Hola, {param}</h1>"
+
+@app.route("/operas")
+def operas():
+    return '''
+    <form>
+        <label>Name:</label>
+        <input type="text"><br>
+        <label>Apellido Paterno:</label>
+        <input type="text"><br>
+        <input type="submit">
+    </form>
+    '''
 
 if __name__ == '__main__':
     app.run(debug=True)
